a3b055c64d7c7ccaa8a16deb9f9080ec92f25392
Lucas Nguyen <lucasnguyen@Lucass-Laptop.local>
2023-02-12 21:30:43 -0800

    Got it to work

---
+++ collator.py
@@ -0,0 +1,10 @@
+import torch
+
+class ImageClassificationCollator:
+    def __init__(self, feature_extractor):
+        self.feature_extractor = feature_extractor
+ 
+    def __call__(self, batch):
+        encodings = self.feature_extractor([x[0] for x in batch], return_tensors='pt')
+        encodings['labels'] = torch.tensor([x[1] for x in batch], dtype=torch.long)
+        return encodings 
